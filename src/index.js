e71cc1b727038d72b2b3e4d625c15e6a55be553a
Alexandr <caiiiia_podoplelov@mail.ru>
2022-04-10 22:44:13 +0300

    fill state api data(randomChar)

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './components/app/App';
+
 import './style/style.scss';
 
-import Request from './services/services';
-
-const requestApi = new Request();
-requestApi.getAllCharacters().then((res) => res.data.results.forEach((char) => console.log(char.name)));
-requestApi.getCharacter(1009148).then((res) => console.log(res));
 
 ReactDOM.render(
   <React.StrictMode>
