6fa8928519d7e62c7e3428e535c1ad6839f938a7
Alexandr <caiiia_podoplelov@mail.ru>
2022-05-11 03:13:11 +0300

    functional RandomChar

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 
 import Request from '../../services/services';
 import CharBlock from './charBlock';
@@ -8,22 +8,33 @@ import Loading from '../../helpers/loading';
 import './randomChar.scss';
 import mjolnir from '../../resources/img/mjolnir.png';
 
-class RandomChar extends Component {
-  state = {
-    char: {},
-    loading: true,
-    error: false
-  };
-  componentDidMount() { this.characterData() };
+const RandomChar = () => {
+  const [ char, setChar ] = useState({});
+  const [ loading, setLoading ] = useState(true);
+  const [ error, setError ] = useState(false);
+
+  useEffect(() => {
+    characterData();
+
+    const timer = setInterval(characterData, 10000);
+
+    return () => { clearInterval(timer); };
+  }, []) ; // eslint-disable-line
 
 
-  onCharLoaded = (char) => this.setState({ char, loading: false });
+  const onCharLoaded = (char) => {
+    setLoading(false);
+    setChar(char);
+  }
 
-  onError = () => this.setState({ loading: false, error: true });
+  const onError = () => {
+    setLoading(false);
+    setError(true);
+  }
 
-  requestApi = new Request();
-  characterData = () => {
-    this.requestApi
+  const requestApi = new Request();
+  const characterData = () => {
+    requestApi
       .getAllCharacters()
       .then((res) => {
         let resArr = [];
@@ -32,46 +43,42 @@ class RandomChar extends Component {
         const randElemArr = Math.floor(Math.random() * resArr.length),
               id = resArr[randElemArr];
 
-        this.setState({ loading: true });
-        this.requestApi
+        setLoading(true);
+        requestApi
           .getCharacter(id)
-          .then(this.onCharLoaded);
+          .then(onCharLoaded);
       })
-      .catch(this.onError);
+      .catch(onError);
   };
 
 
-  render() {
-    const { char, loading, error } = this.state;
+  const content = loading ?
+                  <Loading/> : error ?
+                              <Error/> :
+                              <CharBlock char={ char }/>;
 
-    const content = loading ?
-                    <Loading/> : error ?
-                                <Error/> :
-                                <CharBlock char={ char }/>;
+  return (
+    <div className='randomchar'>
+      { content }
 
-    return (
-      <div className='randomchar'>
-        { content }
+      <div className='randomchar__static'>
+        <p className='randomchar__title'>
+          Random character for today!<br/>
+          Do you want to get to know him better?
+        </p>
 
-        <div className='randomchar__static'>
-          <p className='randomchar__title'>
-            Random character for today!<br/>
-            Do you want to get to know him better?
-          </p>
+        <p className='randomchar__title'>
+          Or choose another one
+        </p>
 
-          <p className='randomchar__title'>
-            Or choose another one
-          </p>
+        <button className='button button__main' onClick={ characterData }>
+          <div className='inner'>try it</div>
+        </button>
 
-          <button className='button button__main' onClick={ this.characterData }>
-            <div className='inner'>try it</div>
-          </button>
-
-          <img src={ mjolnir } alt='mjolnir' className='randomchar__decoration'/>
-        </div>
+        <img src={ mjolnir } alt='mjolnir' className='randomchar__decoration'/>
       </div>
-    );
-  };
+    </div>
+  );
 }
 
 export default RandomChar;
